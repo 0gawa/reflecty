23e4bd6578907edf4ab33b444b4f8e3b9df9c051
0gawa <ogawa.daiki.infratop@gmail.com>
2025-08-14 17:29:22 +0900

    JWTの秘密鍵を作成

--- reflecty-api/app/models/user.rb
+++ reflecty-api/app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-  has_secure_password
+  devise :database_authenticatable, :registerable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
+  # devise-jwtが必須とするからのメソッド
+  def jwt_payload
+  end
 
   has_many :entries, dependent: :destroy
   has_many :templates, dependent: :destroy
